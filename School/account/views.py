2ebb7665f666c0b7db080bd22a3f26b0ac08b237
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2022-07-30 19:11:40 -0400

    first commit

---
+++ School/account/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, login,logout
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    msg = None
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            msg = 'user created'
+            return redirect('login_view')
+        else:
+            msg = 'form is not valid'
+    else:
+        form = SignUpForm()
+    return render(request,'register.html', {'form': form, 'msg': msg})
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    msg = None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_admin:
+                login(request, user)
+                return redirect('index')
+            elif user is not None and user.is_employee:
+                login(request, user)
+                return redirect('index')
+            else:
+                msg= 'invalid credentials'
+        else:
+            msg = 'error validating form'
+    return render(request, 'login.html', {'form': form, 'msg': msg})
+
+
+def LogOut(request):
+    logout(request)
+    return redirect('login_view')
---
+++ question1.py
@@ -0,0 +1,6 @@
+import decimal
+
+def div(a,b,n):
+    return round(a/b, n)
+
+print(div(22,7,9))
